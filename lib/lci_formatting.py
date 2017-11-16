3411fbcab3db47f89291b2b85695df8863facb30
Gustave Coste <gustave.coste@inra.fr>
2017-11-16 16:47:41 +0100

    Refactored lci_formatting.py

--- lib/lci_formatting.py
+++ lib/lci_formatting.py
@@ -9,50 +9,58 @@ from lib.parameters import *
 # SIMAPRO SYSTEM LCI FILES HAVE TO BE EXPORTED WITH THE OPTION "Detail" and "per categories"
 # otherwise, some line are added and the algorithm lost itself !....
 
-# TODO ANTOINE: Describe function in docstring
-def localisation_compartment(lci):
+def compartment_coords(lci, compartments, new_names=None):
+    """
+    Gets the coordinates of given compartments in a Simapro LCI export.
+
+    :param DataFrame lci:
+        Simapro LCI export converted in Pandas DataFrame
+    :param iterable compartments:
+        Iterable constituted by the names of the compartments
+    :param iterable new_names:
+        New compartments names (optional)
+    :return: Dictionnary constituted by compartments names as keys and a tuple made of compartment first and last row as
+        value
+    :rtype: dict
     """
 
+    # If new names is None, assigns compartments to it, otherwise, asserts there are one new name by compartment
+    if new_names is None:
+        new_names = compartments
+    elif len(new_names) != len(compartments):
+        raise Exception("New names number doesn't match compartments number")
 
-    :param lci:
+    # Initializing compartment_dict
+    compartment_dict = dict()
+
+    # Looping over compartments to get their coordinates
+    for compartment in compartments:
+
+        # Getting the number of the first row of the compartment
+        start = lci[lci.iloc[:, 0] == compartment].index[0] + 1
+
+        # Looping on every rows until the first empty one to get the number of the last row of the compartment
+        end = start
+        while pd.notna(lci.iloc[end, 0]):
+            end += 1
+
+        # Sets the compartments name and coordinates
+        compartment_dict[new_names[compartments.index(compartment)]] = (start, end - 1)
+
+    return compartment_dict
+
+
+def extract_lci(lci_simapro):
+    """
+
+    :param lci_simapro:
     :return:
     """
-    v = pd.DataFrame(lci.iloc[:, 0] == 'Resources', columns=["start"])
-    w = pd.DataFrame(lci.iloc[:, 0] == 'Emissions to air', columns=["start"])
-    x = pd.DataFrame(lci.iloc[:, 0] == 'Emissions to water', columns=["start"])
-    z = pd.DataFrame(lci.iloc[:, 0] == 'Emissions to soil', columns=["start"])
+    compartments = compartment_coords(lci_simapro, COMPARTMENTS, NEW_NAMES)
 
-    v = v[(v != 0).all(1)]
-    w = w[(w != 0).all(1)]
-    x = x[(x != 0).all(1)]
-    z = z[(z != 0).all(1)]
-
-    v = v.append(w)
-    v = v.append(x)
-    v = v.append(z)
-
-    v.loc[:, 'start'] = v.index
-
-    v["end"] = [0] * 4
-
-    for i in v.index:
-        j = i
-        while lci.iloc[j, 0] == lci.iloc[j, 0]:
-            j += 1
-        v.loc[i, 'end'] = j
-
-    v.index = ['Raw', 'Air', 'Water', 'Soil']
-
-    return v
-
-
-def extract_lci(lci_simapro, loca):
-    liste_serie = list()
-    liste_df = list()
-
-    # Some elementary flows sub-compartment have to be modified to gat a right nomenclature for rosetta....
-    lci_simapro.loc[lci_simapro.iloc[:, 1] != lci_simapro.iloc[:,
-                                              1], 1] = "(unspecified)"  # if sub-comp is empty, i put unspecified
+    # Some elementary flows sub-compartment have to be modified to get a right nomenclature for rosetta....
+    # If sub-comp is empty, I put unspecified
+    lci_simapro.loc[lci_simapro.iloc[:, 1] != lci_simapro.iloc[:, 1], 1] = "(unspecified)"
     lci_simapro.loc[lci_simapro.iloc[:, 1] == 'in ground', 1] = "(unspecified)"
     lci_simapro.loc[lci_simapro.iloc[:, 1] == 'land', 1] = "(unspecified)"
     lci_simapro.loc[lci_simapro.iloc[:, 1] == 'in air', 1] = "(unspecified)"
@@ -74,59 +82,71 @@ def extract_lci(lci_simapro, loca):
 
     lci_table = pd.DataFrame()
 
-    for i in range(0, loca.shape[0]):
-        elementary_flows = pd.DataFrame(lci_simapro.iloc[loca.iloc[i, 0] + 1:loca.iloc[i, 1], 2])
-        elementary_flows.index = lci_simapro.iloc[loca.iloc[i, 0] + 1:loca.iloc[i, 1], 0] + ' to ' + loca.index[
-            i] + ' ' + lci_simapro.iloc[loca.iloc[i, 0] + 1:loca.iloc[i, 1], 1]
+    for compartment, coords in compartments.items():
+        # TODO: Check that coordinates are OK (no need of +1 or -1)
+        elementary_flows = pd.DataFrame(lci_simapro.iloc[coords[0]:coords[1], 2])
+        elementary_flows.index = "{} to {} {}".format(lci_simapro.iloc[coords[0]:coords[1], 0],
+                                                      compartment,
+                                                      lci_simapro.iloc[coords[0]:coords[1], 1])
         lci_table = lci_table.append(elementary_flows)
 
+    # TODO: What to put here?
     lci_table.columns = [filename]
 
-    return lci_table  # liste_serie est liste de serie, meth_df est liste de dataframe
+    return lci_table
 
 
-rosetta = pd.io.excel.read_excel(ROSETTA, sheetname="ee", header=0, skiprows=None)
+def gather_lcis(lcis, lci_dir):
+    """
 
-# TODO GUS: Refactor path and constants usage
 
-lcis_gathered = pd.DataFrame()
 
-# for each lcis file exported from simapro and named in the list
-# the location of each comparment (resources, air, water, soil) in the excel files are extracted
-# then the elementary flows are gathered in the same dataframe 'lci_table'
-# then all the lci_table are gathered in the same dataframe 'lcis_gathered'
+    :param iterable lcis:
+        List of lcis filenames (without file extension)
+    :param str lci_dir:
+        Folder where all lcis files are gathered
+    :return:
+    :rtype: DataFrame
+    """
+    # for each lcis file exported from simapro and named in the list the location of each comparment
+    # (resources, air, water, soil) in the excel files are extracted then the elementary flows are gathered in the same
+    # dataframe 'lci_table' then all the lci_table are gathered in the same dataframe 'lcis_gathered'
 
-for lci in LCIS:
-    filename = os.path.join(LCI_DIR, lci + ".XLSX")
+    lcis_gathered = pd.DataFrame()
 
-    lci_simapro = pd.io.excel.read_excel(filename, header=None, skiprows=None)
+    for lci in lcis:
+        filename = os.path.join(lci_dir, lci + ".XLSX")
 
-    loca = localisation_compartment(lci_simapro)
-    lci_table = extract_lci(lci_simapro, loca)
+        lci_simapro = pd.io.excel.read_excel(filename, header=None, skiprows=None)
 
-    lcis_gathered = pd.DataFrame.join(lcis_gathered, lci_table, how='outer')
+        lci_table = extract_lci(lci_simapro)
+
+        lcis_gathered = pd.DataFrame.join(lcis_gathered, lci_table, how='outer')
+        lcis_gathered = lcis_gathered.fillna(value=0)
+
+    # Lowercase for the index, otherwise it won't match...
+    lcis_gathered.index = lcis_gathered.index.str.lower()
+
+    # BUT i some dimensions are in double
+    # the duplicate have to be suppressed.
+    # they are here listed
+
+    duplicates = lcis_gathered.index
+
+    duplicates = [item for item, count in collections.Counter(duplicates).items() if count > 1]
+
+    # I don't no if it is the right method but the results on each duplicate dimension are sumed and one of them is
+    # suppressed
+
+    # the two dimensions are considered the same and are sumed
+    # "water to air (unspecified)"
+    # "water/m3 to air (unspecified)"
+
+    for duplicate in duplicates:
+        lcis_gathered.loc[duplicate, :] = [lcis_gathered.loc[duplicate, :].sum()] * \
+                                          (lcis_gathered.loc[duplicate, :].shape[0])
+
     lcis_gathered = lcis_gathered.fillna(value=0)
+    lcis_gathered = lcis_gathered[~lcis_gathered.index.duplicated(keep='first')]
 
-# lowercase for the index, otherwise it won't match...
-lcis_gathered.index = lcis_gathered.index.str.lower()
-
-# BUT i some dimensions are in double
-# the duplicate have to be suppressed.
-# they are here listed
-
-dupli = lcis_gathered.index
-
-dupli = [item for item, count in collections.Counter(dupli).items() if count > 1]
-
-# I don't no if it is the right method but the results on each duplicate dimension are sumed and one of them is suppressed
-
-# the two dimensions are considered the same and are sumed
-# "water to air (unspecified)"
-# "water/m3 to air (unspecified)"
-
-
-for i in dupli:
-    lcis_gathered.loc[i, :] = [lcis_gathered.loc[i, :].sum()] * (lcis_gathered.loc[i, :].shape[0])
-
-lcis_gathered = lcis_gathered.fillna(value=0)
-lcis_gathered = lcis_gathered[~lcis_gathered.index.duplicated(keep='first')]
+    return lcis_gathered
