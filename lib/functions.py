b1ed11463680d84ecf83ca13644f8283d3674b07
Gustave Coste <gustave.coste@inra.fr>
2017-11-17 15:55:41 +0100

    Changed clean_method() behavior
    Doesn't change the input but only returns a cleaned method.

--- lib/functions.py
+++ lib/functions.py
@@ -43,21 +43,22 @@ def clean_method(method_standardized):
     :rtype DataFrame
     """
 
-    method_standardized = method_standardized.fillna(value=0)
+    method_standardized_cleaned = method_standardized.copy(deep=True)
+    method_standardized_cleaned = method_standardized_cleaned.fillna(value=0)
 
     # If a category is empty, deletes it from the method
-    categories = method_standardized.columns.tolist()
+    categories = method_standardized_cleaned.columns.tolist()
     for category in categories:
-        if np.linalg.norm(method_standardized[category]) == 0:
-            method_standardized.drop(category, inplace=True, axis=1)
+        if np.linalg.norm(method_standardized_cleaned[category]) == 0:
+            method_standardized_cleaned.drop(category, inplace=True, axis=1)
 
     # Now it is just a sorting step with the number of CF per category. This will make get more proper orthogonal
     # projection for the next step
-    just_ones = method_standardized.copy(deep=True)
+    just_ones = method_standardized_cleaned.copy(deep=True)
     just_ones[just_ones != 0] = 1
     sum_ones = just_ones.sum(axis=0)
     sum_ones.sort_values(ascending=True, inplace=True)
-    method_standardized_cleaned = method_standardized[sum_ones.index]
+    method_standardized_cleaned = method_standardized_cleaned[sum_ones.index]
 
     return method_standardized_cleaned
 
