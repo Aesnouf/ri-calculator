d8d8acc09ba6460887597caf14ca63d9a210d81c
Gustave Coste <gustave.coste@inra.fr>
2017-11-23 15:22:52 +0100

    Put RI_Calculator.py in a main test

--- RI_Calculator.py
+++ RI_Calculator.py
@@ -30,6 +30,7 @@ LCIS = ['lci1', 'lci2', '...', ]
 # Directory to export the results
 EXPORT_DIR = "Path/to/export/directory"
 
+
 # Uncomment and change these if you want, defaults are ri_category.xlsx and ri_methods.xlsx
 # RI_CATEGORY_FILENAME = "your_filename"
 # RI_METHODS_FILENAME = "your_other_filename"
@@ -37,27 +38,32 @@ EXPORT_DIR = "Path/to/export/directory"
 ########################################################################################################################
 
 # RI CALCULATION
-gmean = pd.io.excel.read_excel(GMEAN)
-methods = pd.io.excel.read_excel(METHODS)
+def main():
+    gmean = pd.io.excel.read_excel(GMEAN)
+    methods = pd.io.excel.read_excel(METHODS)
 
-lcis = gather_lcis(LCIS, LCIS_DIR)
-lcis = lcis.reindex(index=methods.index)
-std_lci = standardize_inventory(lcis, gmean)
-ri_cat = calculate_representativeness_index_per_category(methods, std_lci)
+    lcis = gather_lcis(LCIS, LCIS_DIR)
+    lcis = lcis.reindex(index=methods.index)
+    std_lci = standardize_inventory(lcis, gmean)
+    ri_cat = calculate_representativeness_index_per_category(methods, std_lci)
 
-meth_to_study = methods.loc[:, [s for s in methods.columns if METHODS_LIST[0] in s]]
-ri_methods = calculate_representativeness_index_per_method(meth_to_study, METHODS_LIST[0], std_lci)
+    meth_to_study = methods.loc[:, [s for s in methods.columns if METHODS_LIST[0] in s]]
+    ri_methods = calculate_representativeness_index_per_method(meth_to_study, METHODS_LIST[0], std_lci)
 
-# Exporting data
-try:
-    ri_category_filename = RI_CATEGORY_FILENAME + ".xlsx"
-except NameError:
-    ri_category_filename = "ri_category.xlsx"
+    # Exporting data
+    try:
+        ri_category_filename = RI_CATEGORY_FILENAME + ".xlsx"
+    except NameError:
+        ri_category_filename = "ri_category.xlsx"
 
-try:
-    ri_methods_filename = RI_METHODS_FILENAME + ".xlsx"
-except NameError:
-    ri_methods_filename = "ri_methods.xlsx"
+    try:
+        ri_methods_filename = RI_METHODS_FILENAME + ".xlsx"
+    except NameError:
+        ri_methods_filename = "ri_methods.xlsx"
 
-pd.DataFrame.to_excel(ri_cat, os.path.join(EXPORT_DIR, ri_category_filename))
-pd.DataFrame.to_excel(ri_methods, os.path.join(EXPORT_DIR, ri_methods_filename))
+    pd.DataFrame.to_excel(ri_cat, os.path.join(EXPORT_DIR, ri_category_filename))
+    pd.DataFrame.to_excel(ri_methods, os.path.join(EXPORT_DIR, ri_methods_filename))
+
+
+if __name__ == "__main__":
+    main()
